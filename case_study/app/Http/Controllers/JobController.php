b065963f32e81f5f3d0808917a93d6b41921e1bd
Dustin <d.wilke7755@googlemail.com>
2024-10-15 22:41:03 +0200

    Add functionality to JobController

--- case_study/app/Http/Controllers/JobController.php
+++ case_study/app/Http/Controllers/JobController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Job;
+use App\Models\Category;
+use App\Models\Company;
+use App\Models\User;
 use App\Http\Requests\StoreJobRequest;
 use App\Http\Requests\UpdateJobRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class JobController extends Controller
 {
@@ -13,7 +18,28 @@ class JobController extends Controller
      */
     public function index()
     {
-        //
+        $jobs = DB::table('jobs')
+            ->join('companies', 'jobs.company_id', '=', 'companies.id')
+            ->join('categories', 'jobs.category_id', '=', 'categories.id')
+            ->join('users', 'jobs.user_id', '=', 'users.id')
+            ->select('jobs.id',
+                    'jobs.user_id',
+                    'jobs.title', 
+                    'jobs.description as job_description',
+                    'jobs.created_at',
+                    'jobs.salary', 
+                    'companies.name', 
+                    'companies.email', 
+                    'companies.adress', 
+                    'companies.description as company_description', 
+                    'categories.name', 
+                    'users.prename as user_prename', 
+                    'users.name as user_name')
+            ->get();
+
+        return view('jobs.index', [
+            'jobs' => $jobs
+        ]);
     }
 
     /**
@@ -21,7 +47,18 @@ class JobController extends Controller
      */
     public function create()
     {
-        //
+        $companies = DB::table('companies')
+            ->where('user_id', Auth()->user()->id)
+            ->get();
+
+        $categories = DB::table('categories')
+            ->where('user_id', Auth()->user()->id)
+            ->get();
+
+        return view('jobs.add', [
+            'companies' => $companies,
+            'categories' => $categories
+        ]);
     }
 
     /**
@@ -29,7 +66,17 @@ class JobController extends Controller
      */
     public function store(StoreJobRequest $request)
     {
-        //
+        $job = new Job;
+
+        $job->title = $request->title;
+        $job->description = $request->description;
+        $job->company_id = $request->company;
+        $job->category_id = $request->category;
+        $job->salary = $request->salary;
+        $job->user_id = Auth::user()->id;
+        
+        $job->save();
+        return redirect()->route('jobs.index');
     }
 
     /**
@@ -37,7 +84,29 @@ class JobController extends Controller
      */
     public function show(Job $job)
     {
-        //
+        $job = DB::table('jobs')
+            ->where('jobs.id', $job->id)
+            ->join('companies', 'jobs.company_id', '=', 'companies.id')
+            ->join('categories', 'jobs.category_id', '=', 'categories.id')
+            ->join('users', 'jobs.user_id', '=', 'users.id')
+            ->select('jobs.id',
+                    'jobs.user_id',
+                    'jobs.title', 
+                    'jobs.description as job_description',
+                    'jobs.created_at',
+                    'jobs.salary', 
+                    'companies.name', 
+                    'companies.email', 
+                    'companies.adress', 
+                    'companies.description as company_description', 
+                    'categories.name', 
+                    'users.prename as user_prename', 
+                    'users.name as user_name')
+            ->first();
+
+        return view('jobs.detail', [
+            'job' => $job
+        ]);
     }
 
     /**
@@ -45,7 +114,40 @@ class JobController extends Controller
      */
     public function edit(Job $job)
     {
-        //
+        $job = DB::table('jobs')
+            ->where('jobs.id', $job->id)
+            ->join('companies', 'jobs.company_id', '=', 'companies.id')
+            ->join('categories', 'jobs.category_id', '=', 'categories.id')
+            ->join('users', 'jobs.user_id', '=', 'users.id')
+            ->select('jobs.id',
+                    'jobs.user_id',
+                    'jobs.company_id',
+                    'jobs.category_id',
+                    'jobs.title', 
+                    'jobs.description as job_description',
+                    'jobs.created_at',
+                    'jobs.salary', 
+                    'companies.name as company_name', 
+                    'companies.email', 
+                    'companies.adress', 
+                    'companies.description as company_description', 
+                    'categories.name', 
+                    'users.prename as user_prename', 
+                    'users.name as user_name')
+            ->first();
+        $companies = DB::table('companies')
+            ->where('user_id', Auth()->user()->id)
+            ->get();
+
+        $categories = DB::table('categories')
+            ->where('user_id', Auth()->user()->id)
+            ->get();
+
+        return view('jobs.edit', [
+            'job' => $job,
+            'companies' => $companies,
+            'categories' => $categories    
+        ]);
     }
 
     /**
@@ -53,7 +155,14 @@ class JobController extends Controller
      */
     public function update(UpdateJobRequest $request, Job $job)
     {
-        //
+        $job->title = $request->title;
+        $job->description = $request->description;
+        $job->company_id = $request->company;
+        $job->category_id = $request->category;
+        $job->salary = $request->salary;
+        
+        $job->save();
+        return redirect()->route('jobs.detail', $job);
     }
 
     /**
@@ -61,6 +170,8 @@ class JobController extends Controller
      */
     public function destroy(Job $job)
     {
-        //
+        $job->delete();
+
+        return redirect()->route('jobs.index');
     }
 }
